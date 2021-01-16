eccc46b9920569beabdf89f8b8298c1a07cee228
Scott Malin <mrsemalin@gmail.com>
2021-01-16 05:32:01 -0700

    Created new hompage and swapped all .html to php

--- web/includes/header.php
+++ web/includes/header.php
@@ -1,7 +1,7 @@
 <title>CSE:341 Class Index</title>
 <link rel="shortcut icon" type="image/x-icon" href="./public/images/html.png" /> <!-- Title icon -->
-<link rel="stylesheet" type="text/css" href="./public/stylesheets/main.css">
 <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" />
+<link rel="stylesheet" type="text/css" href="./public/stylesheets/main.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="myscripts.js"></script>
--- web/index.php
+++ web/index.php
@@ -1,12 +1,114 @@
 <!DOCTYPE html>
 <html>
+
+<head>
+   <?php include("../web/includes/header.php");
+   ?>
+   <link rel="stylesheet" type="text/css" href="../web/public/stylesheets/hobbies.css">
+
+</head>
+
 <body>
+   <?php include("../web/includes/nav.php");
+   ?>
+   <br>
+   <div class="top">
+      <h1 class="center_txt">Scott's CSE:341 Projects Home Page</h1>
+      <hr class="hr_top">
+   </div>
+   <br>
 
-<h1>My first PHP page</h1>
+   <div class="middle">
+      <h2 class="center_txt">About Me</h2>
+      <hr class="hr_mid">
+      <!-- Hobbies I Enjoy -->
+      <h3 class="section_title">Hobbies I Enjoy</h3>
+      <div class="grid">
+         <article class="card hobby-item">
+            <header class="card__header">
+               <h1 class="hobby__title">
+                  Fishing
+               </h1>
+            </header>
+            <div class="card__image">
+               <img src="./public/images/fishing.jpg" alt="fishing">
+            </div>
+            <div class="card__content">
+               <p class="hobby__description">
+                  Nothing like catching a large salmon!
+               </p>
+            </div>
+         </article>
+         <article class="card hobby-item">
+            <header class="card__header">
+               <h1 class="hobby__title">
+                  Camping
+               </h1>
+            </header>
+            <div class="card__image">
+               <img src="./public/images/camping.jpg" alt="camping">
+            </div>
+            <div class="card__content">
+               <p class="hobby__description">
+                  Being in nature brings peace to the soul
+               </p>
+            </div>
+         </article>
+         <article class="card hobby-item">
+            <header class="card__header">
+               <h1 class="hobby__title">
+                  Games
+               </h1>
+            </header>
+            <div class="card__image">
+               <img src="./public/images/games.jpg" alt="gamming">
+            </div>
+            <div class="card__content">
+               <p class="hobby__description">
+                  Whether its a board, card, or video game I love playing them all!
+               </p>
+            </div>
+         </article>
+      </div>
 
-<?php
-echo "Hello World!";
-?> 
+      <!-- Sports I Like -->
+      <h3 class="section_title">Sports I Like</h3>
+      <div id="div_sports" class="grid">
+         <article class="card hobby-item">
+            <header class="card__header">
+               <h1 class="hobby__title">
+                  Basketball
+               </h1>
+            </header>
+            <div class="card__image">
+               <img src="./public/images/bb.jpg" alt="basketball">
+            </div>
+            <div class="card__content">
+               <p class="hobby__description">
+                  "Chilling out, maxing, relaxing all cool
+                  And all shooting some b-ball outside of the school"
+               </p>
+            </div>
+         </article>
+         <article class="card hobby-item">
+            <header class="card__header">
+               <h1 class="hobby__title">
+                  Soccer
+               </h1>
+            </header>
+            <div class="card__image">
+               <img src="./public/images/soccer.jpg" alt="soccer">
+            </div>
+            <div class="card__content">
+               <p class="hobby__description">
+                  Not good at it, but it's fun to play!
+               </p>
+            </div>
+         </article>
+      </div>
+   </div>
+
 
 </body>
+
 </html>
