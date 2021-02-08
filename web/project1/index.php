c5aa6fac08eca029f746ebf501106e1ff37a5b72
Scott Malin <mrsemalin@gmail.com>
2021-02-08 05:40:16 -0700

    Fixing Project01 Link try05

--- web/project1/index.php
+++ web/project1/index.php
@@ -3,19 +3,18 @@
 
 
 function my_autoloader($class_name) {
-   if(file_exists(__DIR__ . './classes/' . $class_name . '.php')){
-      require_once __DIR__ . './classes/'. $class_name . '.php';
+   if(file_exists('./classes/' . $class_name . '.php')){
+      require_once './classes/' . $class_name . '.php';
       
-   } 
-   if (file_exists(__DIR__ . './controllers/' . $class_name . '.php')){
-      require_once __DIR__ . './controllers/' . $class_name . '.php';
+   } elseif (file_exists('./controllers/' . $class_name . '.php')){
+      require_once './controllers/' . $class_name . '.php';
    }
 }
 
 spl_autoload_register('my_autoloader');
-//require_once('./controllers/index.php');
-//require_once('./controllers/ClassList.php');
-//require_once('./controllers/ClassSearch.php');
+require_once('./controllers/index.php');
+require_once('./controllers/clientList.php');
+require_once('./controllers/clientSearch.php');
 require_once('./routes/Routes.php');
 
 ?>
