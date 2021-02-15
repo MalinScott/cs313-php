8beddb0b53496b7f14c713b2520980fae6079c20
Scott Malin <mrsemalin@gmail.com>
2021-02-15 11:37:53 -0700

    fixing Navbar link, home link for project01 has been changed back to heroku route and not localhost route

--- web/project1/views/includes/navBar.php
+++ web/project1/views/includes/navBar.php
@@ -1,5 +1,5 @@
 <div class="topnav">
-  <a class=<?php echo ((isset($pageTitle) &&  $pageTitle ==='Keep Track') ? 'active' : 'not_active')?>  href="/cse341_project1/web/project1/index.php">Home</a>
+  <a class=<?php echo ((isset($pageTitle) &&  $pageTitle ==='Keep Track') ? 'active' : 'not_active')?>  href="index.php">Home</a>
   <a class=<?php echo ((isset($pageTitle) &&  $pageTitle ==='Client List') ? 'active' : 'not_active')?>  href="client-list">Client List</a>
   <a class=<?php echo (isset($pageTitle) &&  $pageTitle ==='Client Search') ? 'active' : 'not_active'?>  href="client-search">Client Search</a>
   <a class=<?php echo (isset($pageTitle) &&  $pageTitle ==='Add Clients') ? 'active' : 'not_active'?>  href="client-add">Add Clients</a>
